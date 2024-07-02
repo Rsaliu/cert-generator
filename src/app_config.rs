80f0dee3d4b01974bb484bf0909d6c6c4932eeee
RSaliu <ridwanriley@gmail.com>
2024-07-03 00:52:05 +0100

    Update Appconfig with mqtt details

--- src/app_config.rs
+++ src/app_config.rs
@@ -1,8 +1,10 @@
 
+use crypto_lib::crypto::crypto::CertConfig;
 pub struct AppConfig{
     pub database_url: String,
     pub hmac_key: String,
     pub refresh_token_ttl_in_hr: usize, 
     pub access_token_ttl_in_min: usize, 
     pub activation_token_ttl_in_hr: usize, 
+    pub mqtt_cert_details: CertConfig
 }
